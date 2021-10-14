41cac500b971d246db87535783af4799239d332d
NotANick <ilya.vailov@gmail.com>
2021-10-14 09:15:22 +0300

    Well I've done something

---
+++ src/com/company/Example.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Example
+{
+
+    public static void main(String[] args)
+    {
+        double lit = 10;
+        double gallons = lit / 4.546;
+        System.out.println("in " + lit + " there is " + gallons + " gallons");
+
+    }
+}
